6a1ce8b0a215f85a056009d0abfc2e8dcebf37c4
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-05-30 18:55:52 -0500

    first

---
+++ HealthBuddy/HealthBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthBuddyApp.swift
+//  HealthBuddy
+//
+//  Created by Manuel Meraz on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct HealthBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
