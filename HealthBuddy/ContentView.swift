e593644aadaffa0f3e347c82c3a117ac4a0dd363
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-05-31 02:14:03 -0500

    bc scanning 2

--- HealthBuddy/ContentView.swift
+++ HealthBuddy/ContentView.swift
@@ -9,16 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Text("Scan a document")
+                    .font(.title)
+                    .padding()
+                DocumentScannerView()
+                    .navigationBarTitle("")
+                    .navigationBarHidden(true)
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+
