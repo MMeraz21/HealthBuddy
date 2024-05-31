c1ceeb47955393461fbe8140eaafc6070b9b70f4
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-05-31 02:04:54 -0500

    barcode scanning

---
+++ HealthBuddy/DocumentScannerView.swift
@@ -0,0 +1,18 @@
+//
+//  DocumentScannerView.swift
+//  HealthBuddy
+//
+//  Created by Manuel Meraz on 5/30/24.
+//
+
+import SwiftUI
+
+struct DocumentScannerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DocumentScannerView()
+}
