d9aa463194752ffb1c5e3d825d71e212195980c3
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-06-01 02:22:32 -0500

    fixed parsing

--- HealthBuddy/AddFoodItemView.swift
+++ HealthBuddy/AddFoodItemView.swift
@@ -9,22 +9,116 @@ import SwiftUI
 
 struct AddFoodItemView: View {
     var upc: String
-    var feed =  "https://us.openfoodfacts.org/api/v0/product/0043000208120.json"
+    var feed: String {
+        return "https://us.openfoodfacts.org/api/v0/product/\(upc).json"
+    }
+    @State private var currObj = FoodItem()
+    @State private var pname: String = ""
+    @State private var viewCalories: Int = 0
+    
+    enum SerializationError: Error{
+        case missing(String)
+        case invalid(String, Any)
+    }
     
     func parseSingleObj(){
+        print("parsing obj")
+        guard let feedUrl = URL(string: feed) else {return}
         
+        let request = URLRequest(url: feedUrl)
+        let session = URLSession.shared
+        session.dataTask(with: request){ data, response, error in
+            
+            guard error == nil else{
+                print("error")
+                print(error!.localizedDescription)
+                return
+            }
+            guard let jsonData = data else {
+                print("error with jsonData")
+                return
+            }
+            do{
+                let jsonObj = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                guard let jsonDict = jsonObj as? [String: Any],
+                      let product = jsonDict["product"] as? [String: Any],
+                      let nutriments = product["nutriments"] as? [String:Any]
+                else {
+                    print("Invalid JSON")
+                    return
+                }
+                print(nutriments)
+                
+                let name = product["product_name"] as? String ?? "Unknown"
+                print(name)
+                let brandName = product["brands"] as? String ?? "Unknown"
+                print(brandName)
+                let sugar = nutriments["sugars_100g"] as? Double ?? 0.0
+                print(sugar)
+                let calories = nutriments["energy-kcal_100g"] as? Double ?? 0.0
+                print(calories)
+                let fat = nutriments["fat_100g"] as? Double ?? 0.0
+                print(fat)
+                let protein = nutriments["proteins_100g"] as? Double ?? 0.0
+                print(protein)
+                let carbohydrates = nutriments["carbohydrates_100g"] as? Double ?? 0.0
+                print(carbohydrates)
+                DispatchQueue.main.async{
+                    currObj = FoodItem(productName: name, brandName: brandName, protein: protein, sugar: sugar, calories: calories, carbs: carbohydrates, fat: fat)
+                    pname = currObj.productName
+                    //viewCalories  = currObj.calories
+                }
+
+                
+            }catch{
+                print("JSON Serialization Error:", error)
+            }
+            
+            
+        }.resume()
     }
     
     var body: some View {
         VStack {
             Text("UPC: \(upc)")
-                .foregroundColor(.black) // Explicitly setting text color
+                .foregroundColor(.black)
                 .padding()
+            Text("HELLO???")
+                .foregroundColor(.black)
+                .padding()
+            Text("Name: \(pname)")
+                .foregroundColor(.black)
+                .padding()
+            Text("Calories: \(currObj.calories)")
+                .foregroundColor(.black)
+                .padding()
+            Text("Fat: \(currObj.fat)")
+                .foregroundColor(.black)
+                .padding()
+            Text("Protein: \(currObj.protein)")
+                .foregroundColor(.black)
+                .padding()
+            Text("Carbohydrates: \(currObj.carbs)")
+                .foregroundColor(.black)
+                .padding()
+            Button(action: {
+                print("Blue button pressed")
+            }) {
+                Text("Blue Button")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+            }
+            .padding()
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the view takes up the entire space
-        .background(Color.white) // Explicitly setting background color
-        .edgesIgnoringSafeArea(.all) // Ensure the background color covers the entire view
+        .frame(maxWidth: .infinity, maxHeight: .infinity) //needed to not inherit bg from parent view
+        .background(Color.white)
+        .edgesIgnoringSafeArea(.all)
         .navigationBarHidden(true)
+        .onAppear{
+            parseSingleObj()
+        }
     }
 }
 
--- HealthBuddy/FoodItem.swift
+++ HealthBuddy/FoodItem.swift
@@ -10,10 +10,24 @@ import Foundation
 class FoodItem{
     var productName: String = ""
     var brandName: String = ""
-    var protein: String = ""
-    var sugar: String = ""
-    var calories: String = ""
-    var carbs: String = ""
-    var fat: String = ""
+    var protein: Double = 0.0
+    var sugar: Double = 0.0
+    var calories: Double = 0.0
+    var carbs: Double = 0.0
+    var fat: Double = 0.0
+    
+    init(productName: String, brandName: String, protein: Double, sugar: Double, calories: Double, carbs: Double, fat: Double) {
+        self.productName = productName
+        self.brandName = brandName
+        self.protein = protein
+        self.sugar = sugar
+        self.calories = calories
+        self.carbs = carbs
+        self.fat = fat
+    }
+    
+    convenience init() {
+        self.init(productName: "", brandName: "", protein: 0.0, sugar: 0.0, calories: 0, carbs: 0.0, fat: 0.0)
+    }
     
 }
