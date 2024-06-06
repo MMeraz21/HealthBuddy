113cbc56890fe180ed996f48023a607cf566f3ac
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-06-05 23:41:18 -0500

    Beginning to work on search bar

--- SearchView.swift
+++ SearchView.swift
@@ -12,6 +12,33 @@ struct SearchView: View {
     @State private var searchResults: [FoodItem]?
     @State private var toScannerView: Bool = false
     
+    var feed: String {
+        return "https://us.openfoodfacts.org/cgi/search.pl?search_terms=\(searchText)search_simple=1&action=process&json=1"
+    }
+    
+    func parseFoodList(){
+        guard let feedUrl = URL(string: feed) else {return}
+        
+        let request = URLRequest(url: feedUrl)
+        let session = URLSession.shared
+        
+        session.dataTask(with: request){ data, response, error in
+            
+            guard error == nil else{
+                print("error")
+                print(error!.localizedDescription)
+                return
+            }
+            guard let jsonData = data else {
+                print("error with jsonData")
+                return
+            }
+            
+            
+            
+        }.resume()
+    }
+    
     var body: some View {
         //NavigationView{
             VStack{
@@ -23,6 +50,9 @@ struct SearchView: View {
                         .cornerRadius(8)
                         .overlay(
                             HStack {
+                                Image(systemName: "magnifyingglass")
+                                    .foregroundColor(.gray)
+                                    .padding(.leading, 8)
                                 Spacer()
                                 Image(systemName: "barcode.viewfinder")
                                     .foregroundColor(.gray)
@@ -41,6 +71,8 @@ struct SearchView: View {
                         EmptyView()
                     }
                 )
+                
+                //add search items here
 
             }
         //}
