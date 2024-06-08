2ed1de4dcee4e19332fd43e330d6382339f6d1d1
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-06-08 17:05:44 -0500

    Before createfooditemview

---
+++ HealthBuddy/CreateFoodItemView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateFoodItemView.swift
+//  HealthBuddy
+//
+//  Created by Manuel Meraz on 6/8/24.
+//
+
+import SwiftUI
+
+struct CreateFoodItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateFoodItemView()
+}
