86b0c9d454ba010e25b9d7408922bb75b8a6aa57
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-06-09 23:09:12 -0500

    Starting historyview

---
+++ HealthBuddy/HistoryView.swift
@@ -0,0 +1,18 @@
+//
+//  HistoryView.swift
+//  HealthBuddy
+//
+//  Created by Manuel Meraz on 6/9/24.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HistoryView()
+}
